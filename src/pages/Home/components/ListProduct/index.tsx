466431f568884a5fdf8d55424cc4424b8bca2db8
AdsonTanajura <adson.tanajura@gmail.com>
2024-12-03 11:55:28 -0300

    Feat(JsonServer): Add and configure jsonServer

--- src/pages/Home/components/ListProduct/index.tsx
+++ src/pages/Home/components/ListProduct/index.tsx
@@ -1,48 +1,51 @@
 import ProductCard from '../../../../components/Product Card';
+import axios from 'axios';
 import { ListCotainer } from './styles';
+import { useEffect, useState } from 'react';
+import { DataProps } from './types';
 
 const ListProduct = () => {
+  const [data, setData] = useState<DataProps[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/cards?_page=1&_per_page=8`
+        );
+        const result: DataProps[] = await response.data.data;
+        console.log(result);
+        setData(result);
+      } catch (err) {
+        setError('Erro ao buscar dados');
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (error) {
+    console.log('Tivemos um erro', error);
+  }
+
   return (
     <ListCotainer>
-      <ProductCard
-        description="awdadwadawdwa"
-        img="https://desafiocompass03.s3.us-east-2.amazonaws.com/sectiom3Img3.jpg"
-        price={4000000}
-        title="Teste"
-        isNew
-      />
-
-      <ProductCard
-        description="awdadwadawdwa"
-        img="https://desafiocompass03.s3.us-east-2.amazonaws.com/sectiom3Img3.jpg"
-        price={4000000}
-        title="Teste"
-        isNew
-      />
-
-      <ProductCard
-        description="awdadwadawdwa"
-        img="https://desafiocompass03.s3.us-east-2.amazonaws.com/sectiom3Img3.jpg"
-        price={4000000}
-        title="Teste"
-        isNew
-      />
-
-      <ProductCard
-        description="awdadwadawdwa"
-        img="https://desafiocompass03.s3.us-east-2.amazonaws.com/sectiom3Img3.jpg"
-        price={4000000}
-        title="Teste"
-        isNew
-      />
-
-      <ProductCard
-        description="awdadwadawdwa"
-        img="https://desafiocompass03.s3.us-east-2.amazonaws.com/sectiom3Img3.jpg"
-        price={4000000}
-        title="Teste"
-        isNew
-      />
+      {loading && <h1>Carregando</h1>}
+      {data.map((card) => (
+        <ProductCard
+          description={card.description}
+          img={card.img}
+          price={card.price}
+          title={card.title}
+          discount={card.discount}
+          isNew={card.isNew}
+          key={card.id}
+        />
+      ))}
     </ListCotainer>
   );
 };
---
+++ src/pages/Home/components/ListProduct/types.ts
@@ -0,0 +1,9 @@
+export type DataProps = {
+  id: string;
+  img: string;
+  title: string;
+  description: string;
+  price: number;
+  discount?: number;
+  isNew?: boolean;
+};
