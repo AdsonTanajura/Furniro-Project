16e32baad80b4dd50af106040ae54ddde61b8078
AdsonTanajura <adson.tanajura@gmail.com>
2025-03-18 12:27:02 -0300

    Refactor(Shop): move the logic code to logics.ts file

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,83 +1,35 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
 import FilterBar from './components/FilterBar';
 import { ProductList } from './styles';
-import { DataObjectProps, ProductDataProps } from './types';
-import axios from 'axios';
 import ProductCard from '../../components/Product Card';
 
 import LoadingSpinner from '../../components/LoadingSpinner';
 import Banner from '../../components/Banner';
 import PagesButtons from './components/PagesButtons';
-
-const BASEURL = import.meta.env.VITE_BASE_URL;
+import { useShopLogic } from './logic';
 
 const Shop = () => {
-  const [data, setData] = useState<ProductDataProps[]>([]);
-  const [dataObject, setDataObject] = useState<DataObjectProps>();
-  const [showManyCards, setShowManyCards] = useState('16');
-  const [productsList, setProductsList] = useState<ProductDataProps[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [filters, setFilters] = useState<string[]>([]);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [firstButton, setFirstButton] = useState(1);
-  const [secondButton, setSecondButton] = useState(2);
-  const [thirdButton, setThirdButton] = useState(3);
-
-  const [searchParams] = useSearchParams();
-  useEffect(() => {
-    const paramValue = searchParams.get('category');
-    if (paramValue) {
-      setFilters([paramValue]);
-    }
-  }, [searchParams]);
-
-  useEffect(() => {
-    const filteredProducts =
-      filters.length === 0
-        ? data
-        : data.filter((product) =>
-            filters.every((filter) => product.categories.includes(filter))
-          );
-
-    setProductsList(filteredProducts);
-  }, [data, filters]);
-
-  const fetchData = useCallback(
-    async ({ page = 1, per = '16' }: { page?: number; per?: string }) => {
-      try {
-        setLoading(true);
-        const response = await axios.get(
-          `${BASEURL}/cards?_page=${page}&_per_page=${per}`
-        );
-        const result = response.data.data;
-
-        setDataObject(response.data);
-
-        setData((prevData) => {
-          if (prevData.length === data.length) {
-            return [...prevData, ...result];
-          }
-          return result;
-        });
-      } catch (err) {
-        setError('Erro ao buscar dados');
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    },
-    []
-  );
-
-  useEffect(() => {
-    fetchData({ page: currentPage, per: showManyCards });
-  }, [fetchData, showManyCards, currentPage]);
+  const {
+    currentPage,
+    dataObject,
+    error,
+    filters,
+    firstButton,
+    loading,
+    productsList,
+    secondButton,
+    setCurrentPage,
+    setFilters,
+    setFirstButton,
+    setSecondButton,
+    setShowManyCards,
+    setThirdButton,
+    showManyCards,
+    thirdButton,
+  } = useShopLogic();
 
   if (error) {
-    return <h1>Teve um erro!</h1>;
+    console.log(error);
+    return <h1>Teve um erro! {error}</h1>;
   }
   return (
     <>
---
+++ src/pages/Shop/logic.ts
@@ -0,0 +1,88 @@
+import { useCallback, useEffect, useState } from 'react';
+import { DataObjectProps, ProductDataProps } from './types';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+const BASEURL = import.meta.env.VITE_BASE_URL;
+
+export const useShopLogic = () => {
+  const [data, setData] = useState<ProductDataProps[]>([]);
+  const [dataObject, setDataObject] = useState<DataObjectProps>();
+  const [showManyCards, setShowManyCards] = useState('16');
+  const [productsList, setProductsList] = useState<ProductDataProps[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [filters, setFilters] = useState<string[]>([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [firstButton, setFirstButton] = useState(1);
+  const [secondButton, setSecondButton] = useState(2);
+  const [thirdButton, setThirdButton] = useState(3);
+
+  const [searchParams] = useSearchParams();
+  useEffect(() => {
+    const paramValue = searchParams.get('category');
+    if (paramValue) {
+      setFilters([paramValue]);
+    }
+  }, [searchParams]);
+
+  useEffect(() => {
+    const filteredProducts =
+      filters.length === 0
+        ? data
+        : data.filter((product) =>
+            filters.every((filter) => product.categories.includes(filter))
+          );
+
+    setProductsList(filteredProducts);
+  }, [data, filters]);
+
+  const fetchData = useCallback(
+    async ({ page = 1, per = '16' }: { page?: number; per?: string }) => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${BASEURL}/cards?_page=${page}&_per_page=${per}`
+        );
+        const result = response.data.data;
+
+        setDataObject(response.data);
+
+        setData((prevData) => {
+          if (prevData.length === data.length) {
+            return [...prevData, ...result];
+          }
+          return result;
+        });
+      } catch (err) {
+        setError('Erro ao buscar dados');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
+  useEffect(() => {
+    fetchData({ page: currentPage, per: showManyCards });
+  }, [fetchData, showManyCards, currentPage]);
+
+  return {
+    firstButton,
+    secondButton,
+    thirdButton,
+    filters,
+    productsList,
+    setFilters,
+    setShowManyCards,
+    setFirstButton,
+    setSecondButton,
+    setThirdButton,
+    setCurrentPage,
+    loading,
+    error,
+    dataObject,
+    currentPage,
+    showManyCards,
+  };
+};
