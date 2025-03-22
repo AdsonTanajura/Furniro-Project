0e22982c65899f8a805237f3d68923de9ff70fb5
AdsonTanajura <adson.tanajura@gmail.com>
2025-03-21 22:59:07 -0300

    Test(ErrorPage): add test for ErrorPage component

---
+++ src/components/ErrorPage/ErrorPage.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import ErrorPage from '.';
+
+describe('<ErrorPage />', () => {
+  const { getByRole, getByText, container } = render(<ErrorPage />);
+  it('should be visible to users', () => {
+    const title = getByRole('heading');
+    const emoji = getByText('🙀');
+    const errorMensagem = getByText('Sorry about what happened');
+
+    expect(title).toBeVisible();
+    expect(emoji).toBeVisible();
+    expect(errorMensagem).toBeVisible();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('the error message passed in by parameter must be visible to users', () => {
+    const message = 'my bed error';
+    const { getByRole, getByText, container } = render(
+      <ErrorPage errorMenssage={message} />
+    );
+
+    const title = getByRole('heading');
+    const emoji = getByText('🙀');
+    const errorMensagem = getByText(message);
+
+    expect(title).toBeVisible();
+    expect(emoji).toBeVisible();
+    expect(errorMensagem).toBeVisible();
+    expect(container).toMatchSnapshot();
+  });
+});
